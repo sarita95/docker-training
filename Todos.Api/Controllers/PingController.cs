0714132cdfbda66ac2859e26cac44fc94049a756
Sara <sara.milovic95@gmail.com>
2024-11-29 12:21:28 +0100

    Change name

--- Todos.Api/Controllers/PingController.cs
+++ Todos.Api/Controllers/PingController.cs
@@ -9,7 +9,7 @@ namespace Todos.Api.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            var myName = System.Environment.GetEnvironmentVariable("MY_NAME") ?? "unknown";
+            var myName = System.Environment.GetEnvironmentVariable("MY_NAME") ?? "unknown 1";
             return Ok("pong " + myName);
         }
     }
